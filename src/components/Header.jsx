61ab4fe0a592dbbb4357d6c3c76da25810798e5f
samuel5g <samuelmonteiro21@gmail.com>
2023-01-13 18:54:47 -0300

    feat(component): :sparkles: Finish header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import bgImage from './images/wallpaper.jpeg';
+
+function Header() {
+  return (
+    <header className="flex flex-col leading-6">
+      <p className="text-sm font-medium w-full leading-10 text-center h-10 text-slate-200 bg-[#181818]">Ganhe R$ 10,00 de desconto no frete</p>
+      <p className="text-xl font-medium w-full leading-10 text-center h-10 text-[#181818] bg-[#F9F9F9]">JordanShoes</p>
+      <div className="relative full h-72 bg-black">
+        <img className="absolute h-full w-full object-cover opacity-40" src={bgImage} alt="" />
+        <h1 className="absolute text-4xl font-medium top-20 left-16 text-slate-200">A melhor loja de Jordan</h1>
+        <p className="absolute text-lg font-medium top-40 left-16 w-5/12 text-slate-200">O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
