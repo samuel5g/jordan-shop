91a81879a80149352ae6ea6516924c6ea2c9284d
samuel5g <samuelmonteiro21@gmail.com>
2023-01-13 18:59:11 -0300

    feat(component): :sparkles: Adds magin bottom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import bgImage from './images/wallpaper.jpeg';
 
 function Header() {
   return (
-    <header className="flex flex-col leading-6">
+    <header className="flex flex-col mb-20">
       <p className="text-sm font-medium w-full leading-10 text-center h-10 text-slate-200 bg-[#181818]">Ganhe R$ 10,00 de desconto no frete</p>
       <p className="text-xl font-medium w-full leading-10 text-center h-10 text-[#181818] bg-[#F9F9F9]">JordanShoes</p>
       <div className="relative full h-72 bg-black">
