52c7d5b7ca9fb20233f509d3a60d1735c94bc430
samuel5g <samuelmonteiro21@gmail.com>
2023-01-13 19:41:54 -0300

    chore(assets): :sparkles: Adds archive to import images

---
+++ src/data/imageArray.js
@@ -0,0 +1,12 @@
+import one from '../components/images/1.png';
+import two from '../components/images/2.png';
+import three from '../components/images/3.png';
+import four from '../components/images/4.png';
+import five from '../components/images/5.png';
+import six from '../components/images/6.png';
+import seven from '../components/images/7.png';
+import eight from '../components/images/8.png';
+
+const images = [one, two, three, four, five, six, seven, eight];
+
+export default images;
